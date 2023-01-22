0d9d27ecde3def1ebd6bf04e2c744bb92fece60d
Richard Melkonian <r.v.melkonian@gmail.com>
2023-01-22 23:01:25 +0000

    add validation and error messages

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,33 +1,62 @@
 mod hello_world;
-mod mirror_body_string;
+mod middleware_message;
 mod mirror_body_json;
+mod mirror_body_string;
+mod mirror_custom_header;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
-mod mirror_user_agent;
-mod mirror_custom_header;
+mod read_middleware_custom_header;
+mod set_middleware_custom_header;
+mod always_errors;
+mod returns_201;
+mod get_json;
+mod validate_data;
 
 use axum::{
-    body::Body,
-    routing::{get, post},
     http::Method,
-    Router,
+    middleware,
+    routing::{get, post},
+    Extension, Router,
 };
 
 use hello_world::hello_world;
-use mirror_body_string::mirror_body_string;
+use middleware_message::middleware_message;
 use mirror_body_json::mirror_body_json;
-use path_variables::{path_variables, hard_coded_path};
-use query_params::query_params;
-use mirror_user_agent::mirror_user_agent;
+use mirror_body_string::mirror_body_string;
 use mirror_custom_header::mirror_custom_header;
-use tower_http::cors::{CorsLayer, Any};
+use mirror_user_agent::mirror_user_agent;
+use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
+use read_middleware_custom_header::read_middleware_custom_header;
+use set_middleware_custom_header::set_middleware_custom_header;
+use tower_http::cors::{Any, CorsLayer};
+use always_errors::always_errors;
+use returns_201::returns_201;
+use get_json::get_json;
+use validate_data::validate_data;
 
-pub fn create_routes() -> Router<(), Body> {
+#[derive(Clone)]
+pub struct SharedData {
+    pub message: String,
+}
+
+pub fn create_routes() -> Router {
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
 
-    Router::new().route("/", get(hello_world))
+    let shared_data = SharedData {
+        message: "Hello from shared data".to_owned(),
+    };
+
+    Router::new()
+        .route(
+            "/read_middleware_custom_header",
+            get(read_middleware_custom_header),
+        )
+        .route_layer(middleware::from_fn(set_middleware_custom_header))
+        .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/15", get(hard_coded_path))
@@ -35,5 +64,11 @@ pub fn create_routes() -> Router<(), Body> {
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
         .route("/mirror_custom_header", get(mirror_custom_header))
+        .route("/middleware_message", get(middleware_message))
         .layer(cors)
+        .layer(Extension(shared_data))
+        .route("/always_errors", get(always_errors))
+        .route("/returns_201", post(returns_201))
+        .route("/get_json", get(get_json))
+        .route("/validate_data", post(validate_data))
 }
